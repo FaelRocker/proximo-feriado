6645f722914bdacdaa75b278a682f4f1d9ba3b82
Rafael Alves <rafael.alves.ds@gmail.com>
2020-06-03 19:17:34 -0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@
 import React, { useState } from 'react';
 import {useEffect} from 'react';
 import axios from 'axios';
+import moment from 'moment'
 const TOKEN = "cmFmYWVsLmFsdmVzLmRzQGdtYWlsLmNvbSZoYXNoPTUxODUxNDM0";
 
 interface Feriado{
   nome: string
   data: string
   descricao: string
+  dataFeriado: Date
+  dataHoje: Date
   link: string
+  dif: any
 }
 
 function App() {
@@ -16,11 +20,14 @@ function App() {
     nome: '',
     data: '',
     descricao: '',
-    link: ''
+    dataFeriado: new Date(),
+    dataHoje: new Date(),
+    link: '',
+    dif: ''
   }); 
 
   const hoje:Date = new Date();
-  
+  let diaFeriado:Date = new Date();
   useEffect(() => {
       navigator.geolocation.getCurrentPosition((info: Position) => {
           const latitude = info.coords.latitude.toString();
@@ -36,17 +43,24 @@ function App() {
             console.log(response.statusText);
             
             axios.get(`https://api.calendario.com.br/?json=true&ano=${hoje.getFullYear()}&estado=${estado}&cidade=${cidade}&token=${TOKEN}`).then((response)=>{
-              feriados = response.data;
+              feriados = response.data;             
               const filtrado = feriados.filter(feriado=>{
-                return feriado.type_code !== '9' && new Date(feriado.date.split('/').reverse().join('/')) >= hoje;
+                diaFeriado = new Date(feriado.date.split('/').reverse().join('/'));
+                return feriado.type_code !== '9' && diaFeriado >= hoje;
               });
               
+              let dif = Math.abs(hoje.getTime() - diaFeriado.getTime());
+              console.log(moment(diaFeriado));
               setFeriado({
                 nome: filtrado[0].name,
                 data: filtrado[0].date,
                 descricao: filtrado[0].description,
-                link: filtrado[0].link
-              });             
+                dataFeriado: diaFeriado,
+                dataHoje: hoje,
+                link: filtrado[0].link,
+                dif: Math.ceil(dif / (1000 * 60 * 60 * 24))
+              });   
+              console.log(moment().format());          
 
             }).catch(error=>{
               console.log("Deu ruim.Feriado");
@@ -76,7 +90,7 @@ function App() {
         <h1 className="panel-title">Faltam X semana(s)</h1>
       </div>
       <div>
-        <h1 className="panel-title"> X dias(s)</h1>
+        <h1 className="panel-title"> {`${feriado.dif}`} dias(s)</h1>
       </div>
       <div>
         <h3 className="panel-title"> XX:XX:XX horas(s)</h3>
